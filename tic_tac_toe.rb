0eac0ce2ab279cb4a2564cf165b86cd84fe85039
Saleamlack <saleamlack7@gmail.com>
2023-03-14 19:46:39 +0300

    Use 2 spaces for indentation

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,8 @@
-
 class Player
-    attr_accessor :name, :player_symbol
+  attr_accessor :name, :player_symbol
     
-    def initialize(name, player_symbol)
-        self.name = name
-        self.mark = player_symbol
-    end
+  def initialize(name, player_symbol)
+    self.name = name
+    self.mark = player_symbol
+  end
 end
