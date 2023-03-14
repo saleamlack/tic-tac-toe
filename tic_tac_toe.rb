d58338ccc5f28b719d3a566031e58e5ad8e1fe01
Saleamlack <saleamlack7@gmail.com>
2023-03-14 20:19:09 +0300

    Add method to instantiate two player objects
    The method creates two instances of a Player class by
    prompting the user and symbols of two players. it then
    returns a hash containing the two player objects.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,6 +3,21 @@ class Player
     
   def initialize(name, player_symbol)
     self.name = name
-    self.mark = player_symbol
+    self.player_symbol = player_symbol
   end
 end
+
+def instantiate_player
+  players = {}
+  2.times do |i|
+    puts "Player #{i + 1}: "
+    print "\tName: "
+    name = gets.chomp
+    print "\tSymbol[O/X]: "
+    player_symbol = gets.chomp.upcase
+    players[name] = Player.new(name, player_symbol)
+  end
+  players
+end
+
+players = instantiate_player
