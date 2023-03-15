4361638c813c63551edb182753f19b4a611d1ed0
Saleamlack <saleamlack7@gmail.com>
2023-03-15 18:01:42 +0300

    Add `players` instace variable to `Player` to store objects

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,22 +1,21 @@
 class Player
   attr_accessor :name, :player_symbol
 
+  @@players = {}
   def initialize(name, player_symbol)
     self.name = name
     self.player_symbol = player_symbol
   end
 
   def self.instantiate_player
-    players = {}
     2.times do |i|
       puts "Player #{i + 1}: "
       print "\tName: "
       name = gets.chomp
       print "\tSymbol[O/X]: "
       player_symbol = gets.chomp.upcase
-      players[name] = Player.new(name, player_symbol)
+      @@players[name] = Player.new(name, player_symbol)
     end
-    players
   end
   
   def self.set_current_player
