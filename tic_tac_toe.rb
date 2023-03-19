bba6718f51c0a66dcc6f1966e898a982a763740e
Saleamlack <saleamlack7@gmail.com>
2023-03-19 21:38:53 +0300

    Analyze and format the code by rubocop

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -19,24 +19,24 @@ module TicTacToe
     end
 
     def draw_board
-      self.grid.each_with_index do |symbol, idx|
-        _symbol = symbol ? symbol : idx + 1
-        print "\t" if idx == 0 || idx == 3 || idx == 6
-        print " #{_symbol} "
-        print '|' unless idx == 2 || idx == 5 || idx == 8
-        puts "\n\t---+---+---" if idx == 2 || idx == 5
+      grid.each_with_index do |symbol, idx|
+        print "\t" if [0, 3, 6].include?(idx)
+        print " #{symbol || idx + 1} "
+        print '|' unless [2, 5, 8].include?(idx)
+        puts "\n\t---+---+---" if [2, 5].include?(idx)
         puts if idx == 8
       end
     end
 
     def move(player, position)
-      self.grid[position - 1] = player.player_symbol
+      grid[position - 1] = player.player_symbol
       player.playing_numbers << position
     end
 
     def valid_move?(position)
       return true if grid[position - 1].nil? && (1..9).include?(position)
-      puts "Invalid move. Please select an empty cell on the board"
+
+      puts 'Invalid move. Please select an empty cell on the board'
       false
     end
 
@@ -49,75 +49,78 @@ module TicTacToe
     attr_accessor :player1, :player2, :board, :current_player
 
     def initialize(player1_name, player2_name)
-      self.player1 = Player.new(player1_name, "X")
-      self.player2 = Player.new(player2_name, "O")
+      self.player1 = Player.new(player1_name, 'X')
+      self.player2 = Player.new(player2_name, 'O')
       self.board = Board.new
       self.current_player = nil
     end
 
     def play
-      self.welcome
-      self.set_current_player
-      self.board.draw_board
+      welcome
+      set_current_player
+      board.draw_board
       loop do
-        position = self.set_playing_number
+        position = set_playing_number
         redo unless board.valid_move?(position)
-        self.board.move(self.current_player, position)
-        self.board.draw_board
-        break if self.winner? || self.board.full?
-        self.switch_current_player
+        board.move(current_player, position)
+        board.draw_board
+        break if winner? || board.full?
+
+        switch_current_player
       end
-      puts self.display_result
+      puts display_result
     end
-    
+
     def set_current_player
       puts 'Who plays first?'
-      puts "\t1. #{self.player1.name} (#{self.player1.player_symbol})"
-      puts "\t2. #{self.player2.name} (#{self.player2.player_symbol})"
+      puts "\t1. #{player1.name} (#{player1.player_symbol})"
+      puts "\t2. #{player2.name} (#{player2.player_symbol})"
       print 'Input (1/2): '
       current_player = gets.chomp.to_i
       if valid_player?(current_player)
         self.current_player =
-          current_player == 1 ? self.player1 : self.player2
+          current_player == 1 ? player1 : player2
       else
-        self.set_current_player
+        set_current_player
       end
     end
 
     def switch_current_player
-      self.current_player = 
-        self.current_player == self.player1 ?
-        self.player2 : self.player1
+      self.current_player =
+        if current_player == player1
+          player2
+        else
+          player1
+        end
     end
-  
+
     def set_playing_number
-      puts "Put your symbol on postion: "
-      print "#{self.current_player.name}: "
+      puts 'Put your symbol on postion: '
+      print "#{current_player.name}: "
       played_number = gets.chomp.to_i
-      self.current_player.playing_numbers.push(played_number)
+      current_player.playing_numbers.push(played_number)
       played_number
     end
 
     def winner?
       WINNING_CONDITIONS.each do |line|
-        return true if (line & self.current_player.playing_numbers) == line
+        return true if (line & current_player.playing_numbers) == line
       end
       false
     end
 
     def display_result
-      if self.winner?
-        "#{self.current_player.name}(#{self.current_player.player_symbol}) wins! Congratulations!
-         Thanks for playing!"
-      elsif self.board.full?
-        "It's a tie! Great game.
-         Thanks for playing!"
+      if winner?
+        "#{current_player.name}(#{current_player.player_symbol}) wins! Congratulations! \nThanks for playing!"
+      elsif board.full?
+        "It's a tie! Great game.\nThanks for playing!"
       end
     end
 
     def valid_player?(player)
-      return true if player == 1 || player == 2
-      puts "Invalid player. Please Select player"
+      return true if [1, 2].include?(player)
+
+      puts 'Invalid player. Please Select player'
       false
     end
 
