159b394816eda51a289c08fccfb5f28278c98359
Saleamlack <saleamlack7@gmail.com>
2023-03-19 16:42:50 +0300

    Refactor by separating the game logic and the board state
    We can draw the board based on the state of grid. so it is important to
    separate the board state from the game logic.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -8,6 +8,27 @@ class Player
   end
 end
 
+class Board
+
+  def self.draw_board(board = DEFAULT_BOARD)
+    puts "
+         |     |     
+     -#{board["1"]}- | -#{board["2"]}- | -#{board["3"]}-
+    _____|_____|_____
+         |     |     
+     -#{board["4"]}- | -#{board["5"]}- | -#{board["6"]}- 
+    _____|_____|_____
+         |     |     
+     -#{board["7"]}- | -#{board["8"]}- | -#{board["9"]}- 
+         |     |     "
+  end
+
+  def self.update_board(playing_number)
+    @@new_board[playing_number] = @@players[@@current_player].player_symbol
+    Player.draw_board(@@new_board)
+  end
+end
+
 class Game
   WINNING_CONDITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
   DEFAULT_BOARD = {'1'=>1, '2'=>2, '3'=>3, '4'=>4, '5'=>5, '6'=>6, '7'=>7, '8'=>8, '9'=>9}
@@ -30,19 +51,6 @@ class Game
     self.current_player =
       current_player == 1 ? player_1 : player_2
   end
-  
-  def self.draw_board(board = DEFAULT_BOARD)
-    puts "
-         |     |     
-     -#{board["1"]}- | -#{board["2"]}- | -#{board["3"]}-
-    _____|_____|_____
-         |     |     
-     -#{board["4"]}- | -#{board["5"]}- | -#{board["6"]}- 
-    _____|_____|_____
-         |     |     
-     -#{board["7"]}- | -#{board["8"]}- | -#{board["9"]}- 
-         |     |     "
-  end
  
   def self.set_playing_number(current_player)
     puts "Select a number from a board!"
@@ -50,13 +58,6 @@ class Game
     selected_number = gets.chomp.to_i
     @@players[current_player].playing_numbers.push(selected_number)
   end
-
-  def self.update_board(playing_number)
-    @@new_board[playing_number] = @@players[@@current_player].player_symbol
-    Player.draw_board(@@new_board)
-  end
-
-  private
 
   def winner?(playing_numbers)
     WINNING_CONDITIONS.each do |win|
